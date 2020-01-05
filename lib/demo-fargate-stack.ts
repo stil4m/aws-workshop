a24ca142dc7a555fbee7df016ab8bc6bf53e35ce
Mats Stijlaart <mstijlaart@gmail.com>
2020-01-05 14:03:58 +0100

    Fargate logging

--- lib/demo-fargate-stack.ts
+++ lib/demo-fargate-stack.ts
@@ -1,10 +1,21 @@
 import cdk = require("@aws-cdk/core");
 import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
 import * as ecs from "@aws-cdk/aws-ecs";
+import * as logs from "@aws-cdk/aws-logs";
 
 export class DemoFargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const logGroup = new logs.LogGroup(this, "LogGroup", {
+      retention: logs.RetentionDays.ONE_DAY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    const logDriver = ecs.LogDrivers.awsLogs({
+      streamPrefix: "LoadBalancedService",
+      logGroup
+    });
 
     new ecsPatterns.ApplicationLoadBalancedFargateService(
       this,
@@ -13,7 +24,9 @@ export class DemoFargateStack extends cdk.Stack {
         memoryLimitMiB: 1024,
         cpu: 512,
         taskImageOptions: {
-          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+          image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+          enableLogging: true,
+          logDriver: logDriver
         }
       }
     );
